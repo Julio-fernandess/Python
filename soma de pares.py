8dd8d15060050af6511a202fadf85684a679e34e
Kunnz1 <118297562+Kunnz1@users.noreply.github.com>
2024-11-09 18:36:36 -0300

    Create soma de pares.py

---
+++ soma de pares.py
@@ -0,0 +1,10 @@
+numero = int(input("Digite um numero: "))
+
+soma_pares = 0
+contador = 2 
+
+while contadot <= numero:
+    soma_pares += contador
+    contador += 2
+
+print(f"A soma dos números pares até {numero} é: {soma_pares}")
