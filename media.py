3d0b0dee48ad8bb2bf33b0ab3818ce1f6fabc15b
Kunnz1 <118297562+Kunnz1@users.noreply.github.com>
2024-11-09 19:18:13 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,6 @@
+nota1 = float(input("Digite a Primeira nota: "))
+nota2 = float(input("Digite a Segunda nota: "))
+nota3 = float(input("Digite a Terceira nota: "))
+nota4 = float(input("Digite a Quarta nota: "))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print("A média final obtido: ", media)
